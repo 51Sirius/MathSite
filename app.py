ed2f105f9bbf0dcd1b1a0b1bca885b73730e7f6d
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-05-01 14:31:09 +0300

    create play html

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
+from flask_migrate import Migrate
+from flask_wtf import form
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+import datetime
+import locale
+from os import environ
 
 app = Flask(__name__)
 
@@ -10,7 +16,7 @@ def main_page():
 
 @app.route('/play')
 def play_menu():
-    return render_template('')
+    return render_template('play.html', title='Game')
 
 
 if __name__ == '__main__':
