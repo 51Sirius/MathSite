72a09fea03a98c940a7d0ade202a344b5b4474ad
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-05-03 00:53:32 +0300

    Create generate score

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -2,10 +2,20 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-
-const quest = document.querySelector('.quest')
-quest.innerHTML = String(getRandomInt(10)) + ' + ' + String(getRandomInt(10)) + ' = ?';
+const quest = document.querySelector('.quest');
 
 function generateExample(event) {
-  const level = document.querySelector('.')
+  const level = document.querySelector('.level').textContent;
+  const clas = level[7];
+  const difficulty = level[9]
+    if ('5' === clas) {
+      if ('1' === difficulty) {
+        let number_one = getRandomInt(10)
+        let number_two = getRandomInt(10)
+        quest.innerHTML = String(number_one) + ' + ' + String(number_two) + ' = ?';
+      }
+
+  }
 }
+
+generateExample()
