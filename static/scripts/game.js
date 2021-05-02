6602368f8598314803fb419dadbd83a0d448290c
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-05-02 19:55:40 +0300

    Update stat-bar

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -1,7 +1,11 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
 const quest = document.querySelector('.quest')
 quest.innerHTML = String(getRandomInt(10)) + ' + ' + String(getRandomInt(10)) + ' = ?';
 
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function generateExample(event) {
+  const level = document.querySelector('.')
 }
